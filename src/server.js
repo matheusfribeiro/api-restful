d3674149558ccaff3463a2e337c9bdd8b7e8fb4f
Matheus F. Ribeiro <matheusfribeiro@outlook.com>
2021-10-07 17:07:57 -0300

    uploading CORS middleware

--- src/server.js
+++ src/server.js
@@ -1,13 +1,33 @@
 const express = require('express')
-//const path = require('path')
+const cors = require('cors')
 
 const db = require('./database/db')
 const routes = require('./routes/routes')
+const { application } = require('express')
 
 const app = express()
 
 // Conexão com o banco de dados
 db.connect()
+
+const allowedOrigins = [
+    'http://127.0.0.1:5500',
+    
+]
+
+// habilita cors
+app.use(cors({
+    origin: function(origin, callback) {
+        let allowed = true
+
+        // mobile app
+        if (!origin) allowed = true
+
+        if (!allowedOrigins.includes(origin)) allowed = false
+
+        callback(null, allowed)
+    }
+}))
 
 // Habilita server para receber dados json
 app.use(express.json())
